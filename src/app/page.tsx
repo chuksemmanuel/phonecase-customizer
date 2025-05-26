03315afee97138f766eecdad084d5f0e5a74878a
Chuks Emmanuel <dev.chuksemmanuel@gmail.com>
2025-05-26 16:11:51 +0100

    add shadcn and lucide-react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { Check } from 'lucide-react';
+import Image from 'next/image';
+
 export default function Home() {
-	return <div className='bg-slate-50'></div>;
+	return (
+		<div className='bg-slate-50'>
+			<section>
+				<MaxWidthWrapper classNames='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gapx-8 lg:pt-24 xl:pt-32 lg:pb-52'>
+					<div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
+						<div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start '>
+							<div>
+								<div className='absolute w-28 left-0 -top-20 hidden lg:block'>
+									<Image src='/snake-1.png' alt='snake illustration' width={50} height={50} sizes='100vw'></Image>
+								</div>
+
+								<h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
+									Your Image on a <span className='bg-green-600 px-2 text-white'>Custom</span> Phone Case
+								</h1>
+
+								<p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap '>
+									Capture your favourite memories with your own , <span>one-of-one</span> phone case. CaseCobra allows you to protect your memories,
+									not just your phone case.
+								</p>
+
+								<ul className='mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start'>
+									<div className='space-y-2'>
+										<li className='flex gap-1.5 items-center text-left'>
+											<Check className='size-5 shrink-0 text-green-600' />
+											High-quality, durable material
+										</li>
+									</div>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</MaxWidthWrapper>
+			</section>
+		</div>
+	);
 }
