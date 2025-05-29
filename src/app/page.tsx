fe4d7babe70de916f8545f4689ef36cee6963ff7
Chuks Emmanuel <dev.chuksemmanuel@gmail.com>
2025-05-29 09:39:09 +0100

    hotfix: add margin bottom to last usp list on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -210,7 +210,7 @@ export default function Home() {
 						</div>
 					</div>
 
-					<ul className='mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit'>
+					<ul className='mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit mb-12'>
 						<li className='w-fit'>
 							<Check className='h-5 w-5 text-green-600 inline mr-1.5' />
 							High-quality silicone material
